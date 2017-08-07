4fbec27991bf1e188c09ab2082a6150a34a7758a
PriyaIMAD <30701726+PriyaIMAD@users.noreply.github.com>
2017-08-07 13:16:50 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function( req, res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[aticleName]));
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
