99a2c905c2c186b477b3cdaed7c7e1a6e8091019
PriyaIMAD <30701726+PriyaIMAD@users.noreply.github.com>
2017-08-08 15:23:49 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,8 @@
-console.log('Loaded!');
-
-//change the text
-
-var element = document.getElementById("main-text");
-element.innerHTML = "";
-
-//move image
-
-var img = document.getElementById("madi");
-var marginLeft = 0;
-
-function moveRight()
-{
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
+var button = document.getElementById("counter");
+var counter = 0;
+button.onClick = function(){
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 }
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
     
-};
