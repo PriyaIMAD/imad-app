71ffd94c507dc38da1017d092422dcb41233ad5b
PriyaIMAD <30701726+PriyaIMAD@users.noreply.github.com>
2017-08-12 10:00:58 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ submit.onclick = function(){
         list = '<li>'+ names[i]+'</li>';
     }
     
-    var ul = document.getElementById('nameList');
+    var ul = document.getElementById('namelist');
     ul.innerHTML = list;
     
 }
