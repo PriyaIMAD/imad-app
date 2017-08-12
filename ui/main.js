a73676ec17417f170a4548f40c386f38a4e9d6af
PriyaIMAD <30701726+PriyaIMAD@users.noreply.github.com>
2017-08-12 10:00:23 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,4 +20,22 @@ button.onclick = function(){
     request.open('GET', "http://priyabhandari.imad.hasura-app.io/counter", true);
     request.send(null);
 };
+ 
+ 
+//Submit name
+
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    var names = [name1, name2, name3];
+    var list = '';
+    for(var i=0; i<names.length; i ++){
+        
+        list = '<li>'+ names[i]+'</li>';
+    }
     
+    var ul = document.getElementById('nameList');
+    ul.innerHTML = list;
+    
+}
