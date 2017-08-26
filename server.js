8ebea30a2f84741ca1e2bc211237123b30314043
PriyaIMAD <30701726+PriyaIMAD@users.noreply.github.com>
2017-08-26 07:28:06 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/articles/:articleName', function( req, res){
    
     pool.query("select * from article where title =" + req.params.articleName, function(err, result){
         if (err){
-            res.status(500).send(err.tostring());
+            res.status(500).send(err.toString());
         }else{
             if(result.rows.length === 0){
                 res.status(404).send('Article not found');
