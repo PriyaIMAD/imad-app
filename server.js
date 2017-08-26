73ba16f9ef7185d160a4cf7213eeddc2655616bf
PriyaIMAD <30701726+PriyaIMAD@users.noreply.github.com>
2017-08-26 07:49:57 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.get('/submit-name/', function(req, res){
 app.get('/articles/:articleName', function( req, res){
    
    //select * from article where title = ''; delete where a = 'asdf'
-    pool.query("select * from article where title ='" + req.params.articleName + "'", function(err, result){
+    pool.query("select * from article where title =$1", [req.params.articleName], function(err, result){
         if (err){
             res.status(500).send(err.toString());
         }else{
