c732ebf9161aa32693b4ea9927d84e41e1a6c968
PriyaIMAD <30701726+PriyaIMAD@users.noreply.github.com>
2017-08-29 13:02:21 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require("express-session");
 
 var config = {
     user:'priyabhandari',
@@ -15,6 +16,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+   secret:"somerandomsecretvalue",
+   cookie:{newAge: 10000*60*60*24*30}
+}));
 
 function createTemplate(data){
     var title= data.title;
@@ -98,6 +103,8 @@ app.post('/login', function(req, res){
                var hashedPassword = hash(dbString, salt);
                if(hashedPassword === dbString)
                {
+                   //set the session
+                   req.session.auth = {userid: result.rows[0].id};
                    res.send('credentials correct');
                }else{
                    res.status(403).send('username/password is invalid');
@@ -106,6 +113,16 @@ app.post('/login', function(req, res){
            }
        } 
     });
+});
+
+app.get('/check-login', function(req, res){
+   if(req.session && req.session.auth && req.session.auth.userId)
+   {
+       res.send("You are logged in:" + req.session.auth.userId.toString() );
+   }else{
+       res.send("Your are not logged in");
+   }
+   
 });
 
 app.get('/hash/:input', function(req, res){
